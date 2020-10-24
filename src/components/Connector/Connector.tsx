3850c744451fe0d5aff890f681bb824f37910ed2
Sanich <sancho@cdrpro.ru>
2020-10-24 13:08:39 +0700

    Added thickness for Connector

--- src/components/Connector/Connector.tsx
+++ src/components/Connector/Connector.tsx
@@ -1,22 +1,29 @@
 import { IConnector } from 'relatives-tree/lib/types';
 
+const MIN_THICKNESS = 1;
+
 interface IProps {
   connector: IConnector;
   width: number;
   height: number;
+  thickness?: number;
 }
 
-function Connector({ connector, width, height }: IProps) {
+function calcSide(size: number, factor: number, thickness: number): number {
+  return Math.max(MIN_THICKNESS, size * factor + thickness);
+}
+
+function Connector({ connector, width, height, thickness = MIN_THICKNESS }: IProps) {
   const [x1, y1, x2, y2] = connector.points;
 
   return (
     <i
       style={{
         position: 'absolute',
-        width: `${Math.max(1, (x2 - x1) * width + 1)}px`,
-        height: `${Math.max(1, (y2 - y1) * height + 1)}px`,
+        width: `${calcSide(x2 - x1, width, thickness)}px`,
+        height: `${calcSide(y2 - y1, height, thickness)}px`,
         background: `#999`,
-        transform: `translate(${x1 * width}px, ${y1 * height}px)`,
+        transform: `translate(${x1 * width - (thickness / 2)}px, ${y1 * height - (thickness / 2)}px)`,
         pointerEvents: 'none',
       }}
     />
